186f02c08792c724027818b9f51d45fb904610d2
John Ling <johnlingbusiness@gmail.com>
2025-07-04 21:42:24 +0800

    Added debounced save to journal

--- components/journal/journal_page.tsx
+++ components/journal/journal_page.tsx
@@ -1,10 +1,34 @@
 "use client";
+
 import Menubar from "../menubar/menubar";
 import { Textarea } from "../ui/textarea";
 
+import { ChangeEvent, useEffect, useState } from "react";
 
 export default function JournalPage() {
+  const [content, setContent] = useState<string>("");
+  const [saved, setSaved] = useState<string>("");
+  const [pendingSave, setPendingSave] = useState<boolean>(false);
+
   const currentDate: string = new Date().toLocaleDateString();
+
+  useEffect(() => {
+    // attach event listener for autosave
+    const interval = setInterval(autosave, 1000);
+    return (() => {
+      clearInterval(interval);
+    })
+  })
+
+  function autosave() {
+    if (content !== saved && pendingSave) {
+      // run save code
+      setSaved(content);
+      setPendingSave(false);
+    } else {
+      setPendingSave(true);
+    }
+  }
 
   return (
     <>
@@ -12,7 +36,7 @@ export default function JournalPage() {
         <Menubar />
         <div className="w-3/5">
           <h1 className="p-2">{currentDate}</h1>
-          <Textarea autoCorrect="false" placeholder="What's on your mind?"  className="h-[85vh]"/>
+          <Textarea onChange={(e) => {setContent(e.target.value)}} autoCorrect="false" placeholder="What's on your mind?"  className="h-[85vh]"/>
         </div>    
       </div> 
     </>
