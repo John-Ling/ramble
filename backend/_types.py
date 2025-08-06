29571f2be1d4ffd9154f38665e945da4b4b79adb
John Ling <johnlingbusiness@gmail.com>
2025-08-07 08:52:52 +1000

    feat: Added redis docker container

---
+++ backend/_types.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, BeforeValidator, Field
+from typing import Optional, Annotated, List
+
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class Token(BaseModel):
+    accessToken: str
+    type: str
+
+
+class JournalEntryReference(BaseModel):
+    created: Optional[PyObjectId] = Field(alias="_id", default=None)
+    name: str = ""
+    favourite: bool = False
+
+# Maybe change to journal entry content later
+class JournalEntry(BaseModel):
+    id:  Optional[PyObjectId] = Field(alias="_id", default=None) 
+    authorID: PyObjectId
+    created: PyObjectId
+    content: str = ""
+
+class UpdateJournalEntry(BaseModel):
+    content: Optional[str] = None
+
+class User(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    entries: List[JournalEntryReference]
+
+
+class AccessToken(BaseModel):
+    sub: str
+    token: str
---
+++ backend/app.py
@@ -0,0 +1,305 @@
+from fastapi import Body, Depends, FastAPI, HTTPException, Response, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials, OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import ASCENDING
+from contextlib import asynccontextmanager
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
+from dotenv import load_dotenv
+import httpx
+import os
+import uvicorn
+import redis
+
+from _types import *
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+SECRET = os.getenv("AUTH_SECRET") # Same as NEXTAUTH_SECRET. Used for decrypting JWT
+ALGORITHM=os.getenv("AUTH_ALGORITHM")
+
+db = None
+accessTokens: redis.Redis | None = None # startup redis using docker
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
+bearerScheme = HTTPBearer()
+
+
+userCollection: AsyncIOMotorCollection | None = None
+entryCollection: AsyncIOMotorCollection| None = None
+collection:  AsyncIOMotorCollection | None = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global db, userCollection, entryCollection, accessTokens
+    
+    try:
+        # Startup code
+        client = AsyncIOMotorClient(MONGODB_URI)
+        accessTokens = redis.Redis(decode_responses=True)
+        db = client["prod"]
+        userCollection = db.get_collection("users")
+        entryCollection = db.get_collection("entries")
+
+        # Create indexes for composite keys on entries
+        await entryCollection.create_index([("authorID", ASCENDING), ("created", ASCENDING)], unique=True)
+    except Exception as e:
+        print(e)
+    yield
+    # Shutdown code
+    if db is not None:
+        db.client.close()
+        print("Disconnected from DB")
+
+app = FastAPI(
+    title="Ramble Middleware",
+    description="wagwan",
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/api/auth/set-access-token/", status_code=status.HTTP_201_CREATED)
+async def set_access_token(accessToken: AccessToken):
+    if accessTokens is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Redis is not running"
+        )
+    
+    sub = accessToken.sub 
+    token = accessToken.token
+
+    if accessTokens.get(sub) == None:
+        accessTokens.set(sub, token)
+        return {"message": "Added token"}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Token already exists"
+        )
+        
+@app.post("/api/token/verify/")
+async def verify_token(credentials: HTTPAuthorizationCredentials = Depends(bearerScheme)):
+    if ALGORITHM is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Algorithm is not set"
+        )
+    
+    token = credentials.credentials
+    print(token)
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://oauth2.googleapis.com/tokeninfo?access_token={token}")
+        print(response.json())
+
+@app.get("/api/tokens/")
+async def get_tokens():
+    print(accessTokens)
+    return {"message": accessTokens}
+
+@app.post("/api/users/createEntry/", status_code=status.HTTP_201_CREATED)
+async def create_entry_reference_and_insert_entry(entry: JournalEntry = Body(...)):
+    if userCollection is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="User collection is Null"
+        )
+    
+    uid = entry.authorID
+    user = await userCollection.find_one({"_id": uid})
+    entryDict = entry.model_dump(by_alias=True)
+
+    if (entryDict["created"] == "" or entryDict["_id"] == "" or entryDict["authorID"] == ""):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Malformed request"
+        )
+    
+    # Insert entry reference
+    entryReference: JournalEntryReference = JournalEntryReference() 
+    entryReference.created = entryDict["created"]
+    entryReferenceDict = entryReference.model_dump(by_alias=True)
+
+    if user is not None:
+        await userCollection.update_one({"_id": uid}, {"$push": {"entries": entryReferenceDict}})
+    else:
+        newUser = {
+            "_id": uid,
+            "entries": [entryReferenceDict]
+        }
+        await userCollection.insert_one(newUser)
+    
+    # Insert actual entry into database
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(f"http://localhost:8000/api/entries/insertEntry/", json=entryDict)
+            response.raise_for_status()
+            return {"message": "Everything done :)"}
+        except httpx.HTTPError:
+            # Rollback transaction and remove entry reference
+            if user is not None:
+                await userCollection.update_one({"_id": uid}, {"$pop": {"entries": 1}})
+            else:
+                await userCollection.find_one_and_delete({"_id": uid})
+            
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="walao something go wrong undoing change"
+            )
+
+@app.post("/api/entries/insertEntry/", status_code=status.HTTP_201_CREATED)
+async def insert_entry(response: Response, entry: dict = Body(...)):
+    if entryCollection is None:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        raise httpx.HTTPError(
+            message="Entry collection is Null"
+        )
+    
+    if (entry["created"] == "" or entry["_id"] == "" or entry["authorID"] == ""):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        raise httpx.HTTPError(
+            message="Malformed request"
+        )
+
+    existingEntry = await entryCollection.find_one({"authorID": entry["authorID"], "created": entry["created"]})
+    if existingEntry is not None:
+        response.status_code = status.HTTP_409_CONFLICT
+        raise httpx.HTTPError(
+            message="Entry already exists"
+        )
+
+    await entryCollection.insert_one(entry)
+    return { "message": "Wrote document" }
+    
+@app.get("/api/entries/{uid}/", status_code=status.HTTP_200_OK)
+async def get_entry_references(uid: str, dbDate: str, fetchCount: int = 12):
+    if userCollection is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="User collection is Null"
+        )
+
+    try:       
+        pipeline = [
+            {"$match": {"_id": uid}},
+            {"$unwind": {"path": "$entries", "includeArrayIndex": "entryIndex"}},
+            {
+                # Convert entries created attribute into valid dates for comparisons
+                "$addFields": {
+                    "entries._dateCreated": {
+                        "$dateFromString": { "dateString": "$entries._id" }
+                    }
+                }
+            },
+            {"$sort": {"entries._dateCreated": 1}},    
+            {
+                "$addFields": {
+                    "entries._beforeTarget": {
+                        "$lt": ["$entries._dateCreated", {
+                            "$dateFromString": { "dateString": dbDate }
+                        }]
+                    }
+                }
+            },
+            {
+                "$match": {
+                    "entries._beforeTarget": True
+                }
+            },
+            {"$sort": {"entries._dateCreated": -1}},
+            {"$limit": fetchCount + 1},  
+            {
+                "$group": {
+                    "_id": "$_id",
+                    "entries": {"$push": "$entries"},
+                    "entryCount": {"$sum": 1}
+                }
+            } 
+        ]
+
+        result = await userCollection.aggregate(pipeline).to_list(1)
+        if not result:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="User does not exist or no entries found"
+            )
+
+        entries = result[0].get("entries", [])        
+        finalEntry = entries.pop()
+        count = result[0].get("entryCount", int)
+        return {"entries": entries, "finalEntry": finalEntry, "entryCount": max(0, count - 1)}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error: {e}"
+        )
+
+@app.get("/api/entries/{uid}/{dbDate}/", status_code=status.HTTP_200_OK)
+async def get_entry(uid: str, dbDate: str, credentials: HTTPAuthorizationCredentials = Depends(bearerScheme)):
+    if entryCollection is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Entry collection is Null"
+        )
+
+    if accessTokens is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Redis is not running"
+        )
+    
+    accessToken = credentials.credentials
+    print(accessToken)
+
+    # check if access token exists
+    if accessTokens.get(uid) == None or accessTokens.get(uid) != accessToken:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not authorised"
+        )
+
+    entry = await entryCollection.find_one({"authorID": uid, "created": dbDate})
+
+    if entry is not None:
+        return {"entry": entry}
+    return {"entry": None}
+
+
+@app.put("/api/entries/{uid}/{dbDate}/", status_code=status.HTTP_200_OK)
+async def update_entry(uid: str, dbDate: str, updated: UpdateJournalEntry = Body(...)):
+    if entryCollection is None or userCollection is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Entry or User collection is Null"
+        )
+
+    writeEntry = {
+        k: v for k, v in updated.model_dump(by_alias=True).items() if v is not None
+    }
+
+    if len(writeEntry) >= 1:
+        # at least one field needs to be updated
+        # update entries collection
+        updateResult = await entryCollection.find_one_and_update({"authorID": uid, "created": dbDate}, {"$set": writeEntry})
+        if updateResult is not None:
+            return { "message": "yippie"}
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Document does not exist"
+        )
+    
+
+@app.get("/api/ping/", status_code=status.HTTP_200_OK)
+async def ping():
+    return {"Pong"}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True )
